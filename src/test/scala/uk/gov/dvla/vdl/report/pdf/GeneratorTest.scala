7c7a9e66d7a72980ee2658ad8446c65bdc7079b6
Damian Dunajski <d.dunajski@kainos.com>
2014-11-13 11:41:35 +0000

    [US2479] PDF/A support for licence summary PDF

--- src/test/scala/uk/gov/dvla/vdl/report/pdf/GeneratorTest.scala
+++ src/test/scala/uk/gov/dvla/vdl/report/pdf/GeneratorTest.scala
@@ -29,20 +29,50 @@ class GeneratorTest extends FlatSpec with Matchers {
   it should "throw exception if trying generate report without prior template compilation" in {
     intercept[NoCompiledTemplateException] {
       generator.generate(
-        new JsonReport(reportName, fromFile(resourcePath("data/sample.json")).mkString)
+        new JsonReport(reportName, fromFile(resourcePath(s"data/$reportName.json")).mkString)
       )
     }
   }
 
-  it should "generate valid PDF printout from data source and parameters" in {
+  it should "generate PDF from data source and parameters" in {
 
-    generator.compile(reportName, resourceAsStream("reports/sample.jrxml"))
+    generator.compile(reportName, resourceAsStream(s"reports/$reportName.jrxml"))
 
     val printout: Array[Byte] = generator.generate(
-      new JsonReport(reportName, fromFile(resourcePath("data/sample.json")).mkString,
+      new JsonReport(reportName, fromFile(resourcePath(s"data/$reportName.json")).mkString,
         "GENERATED_BY" -> "Kainos Software Ltd",
         "GENERATION_DATE" -> new Date()
       )
+    )
+
+    printout.length should not be 0
+  }
+
+  it should "generate PDF/A 1A from data source and parameters" in {
+
+    generator.compile(reportName, resourceAsStream(s"reports/$reportName.jrxml"))
+
+    val printout: Array[Byte] = generator.generate(
+      new JsonReport(reportName, fromFile(resourcePath(s"data/$reportName.json")).mkString,
+        "GENERATED_BY" -> "Kainos Software Ltd",
+        "GENERATION_DATE" -> new Date()
+      ),
+      A1AConformance
+    )
+
+    printout.length should not be 0
+  }
+
+  it should "generate PDF/A 1B from data source and parameters" in {
+
+    generator.compile(reportName, resourceAsStream(s"reports/$reportName.jrxml"))
+
+    val printout: Array[Byte] = generator.generate(
+      new JsonReport(reportName, fromFile(resourcePath(s"data/$reportName.json")).mkString,
+        "GENERATED_BY" -> "Kainos Software Ltd",
+        "GENERATION_DATE" -> new Date()
+      ),
+      A1BConformance
     )
 
     printout.length should not be 0
